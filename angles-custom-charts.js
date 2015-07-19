4004057e84a2b0220ab3d6dc8f0b4796879606a9
Sebastian Gonzalez <sebagonz91@gmail.com>
2015-07-19 16:12:02 -0300

    Compile into js

--- angles-custom-charts.js
+++ angles-custom-charts.js
@@ -8,7 +8,7 @@ Chart.types.Line.extend({
   draw: function() {
     var delta, i, j, len, len1, line, point, ref, ref1, scale;
     Chart.types.Line.prototype.draw.apply(this, arguments);
-    if (this.options.vlines.length > 0) {
+    if (this.options.vlines !== void 0 && this.options.vlines.length > 0) {
       ref = this.options.vlines;
       for (i = 0, len = ref.length; i < len; i++) {
         line = ref[i];
@@ -21,23 +21,23 @@ Chart.types.Line.extend({
           this.chart.ctx.lineTo(point.x, scale.endPoint);
           this.chart.ctx.stroke();
           this.chart.ctx.textAlign = 'center';
+          this.chart.ctx.fillStyle = line.lineColor || '#666';
           this.chart.ctx.fillText(line.lineText, point.x, scale.startPoint + 12);
         }
       }
     }
-    if (this.options.hlines.length > 0) {
+    if (this.options.hlines !== void 0 && this.options.hlines.length > 0) {
       ref1 = this.options.hvlines;
       for (j = 0, len1 = ref1.length; j < len1; j++) {
         line = ref1[j];
         if (this.scale.min < line.lineAt && this.scale.max > line.lineAt) {
           scale = this.scale;
           delta = (scale.endPoint - scale.startPoint) / (scale.max - scale.min);
-          console.log(delta);
           this.chart.ctx.beginPath();
           this.chart.ctx.moveTo(scale.xScalePaddingLeft, scale.endPoint - delta * line.lineAt);
           this.chart.ctx.lineTo(scale.width - scale.xScalePaddingRight, scale.endPoint - delta * line.lineAt);
-          this.chart.ctx.lineWidth = line.lineWidth;
-          this.chart.ctx.strokeStyle = line.lineColor;
+          this.chart.ctx.lineWidth = line.lineWidth || 1;
+          this.chart.ctx.strokeStyle = line.lineColor || '#666';
           this.chart.ctx.stroke();
         }
       }
